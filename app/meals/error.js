5f513577452eda6dc496f9cf4fd92c77b8b69086
sinnermann <ealijev@gmail.com>
2024-02-07 20:20:39 +0400

    Second Commit - loading page & sustense component

---
+++ app/meals/error.js
@@ -0,0 +1,11 @@
+"use client"
+
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>An Error Occurred!</h1>
+      <p>Failed to fetch meal data! Try again please...</p>
+    </main>
+  )
+}
---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Unfortunately, the page requesed could not be found...</p>
+    </main>
+  )
+}
